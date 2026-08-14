ed2e05236742e7483c1f19d7de166de83d95093c
Julio Yáñez <julio.yanez@ug.uchile.cl>
2026-08-14 17:15:27 -0400

    fix(creator): fixed syntax errors

--- creator.py
+++ creator.py
@@ -4,7 +4,7 @@ def create_HTTP_message(message):
     head = message["HEAD"]
     body = message["BODY"]
 
-    to_encode = head + "\r\n\r\n" + body
+    to_encode = head + "\r\n\r\n" + body + "\r\n\r\n"
     encoded = to_encode.encode()
 
     return encoded
