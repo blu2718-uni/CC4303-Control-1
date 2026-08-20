48ad21b07d3736c248e560cd5f52dacc4e437d31
Julio Yáñez <julio.yanez@ug.uchile.cl>
2026-08-20 12:40:56 -0400

    chore(message): deleted message.py, as is now unused

--- message.py
+++
@@ -1,4 +0,0 @@
-server_response = message = {
-    "HEAD":"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length: 244",
-    "BODY":'<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta name="viewport" content="width=device-width, initial-scale=1.0"><title>My Simple Page</title></head><body><h1>Hello, world!</h1><p>This is a simple HTML page.</p></body></html>'
-}
