65a9709064837ffa1544ab69efb1048e9047ab33
Julio Yáñez <julio.yanez@ug.uchile.cl>
2026-08-25 11:38:26 -0400

    feat(server): the server is now a proxy

--- server.py
+++ server.py
@@ -4,9 +4,12 @@ import sys
 import creator
 import parser
 
-SERVER_IP="172.20.10.2"
+SERVER_IP="arenarium"
 SERVER_PORT=8000
 BUFFER_SIZE=5
+
+PROXY_HOST="example.com"
+PROXY_PORT=80
 
 with open("html_response.html", "r") as file:
     html = file.read()
@@ -27,35 +30,42 @@ if __name__ == "__main__":
     buffer_size = BUFFER_SIZE
     new_socket_address = (SERVER_IP, SERVER_PORT)
 
-    print('Creando socket - Servidor')
+    print('Creando sockets')
+    proxy_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.bind(new_socket_address)
     server_socket.listen(3)
 
-    json_name = "json_nombre.json"
-    json_address = ""
+    # json_name = "json_nombre.json"
+    # json_address = ""
 
-    if len(sys.argv) == 2:
-        json_name = sys.argv[1]
-    elif len(sys.argv) == 3:
-        json_name = sys.argv[1]
-        json_address = sys.argv[2]
-    elif len(sys.argv) > 3:
-        print("Mas argumentos que lo esperado, usando valores por defecto")
+    # if len(sys.argv) == 2:
+    #     json_name = sys.argv[1]
+    # elif len(sys.argv) == 3:
+    #     json_name = sys.argv[1]
+    #     json_address = sys.argv[2]
+    # elif len(sys.argv) > 3:
+    #     print("Mas argumentos que lo esperado, usando valores por defecto")
 
-    with open(json_address + json_name) as file:
-        name = json.load(file)["nombre"]
+    # with open(json_address + json_name) as file:
+        # name = json.load(file)["nombre"]
 
     while True:
         new_socket, new_socket_address = server_socket.accept()
+        proxy_socket.connect((PROXY_HOST, PROXY_PORT))
+        
         recv_message = receive_full_message(new_socket, buffer_size)
         
         print(f' -> Se ha recibido el siguiente mensaje: {recv_message["HEAD"]}')
-        response_message = server_response
-        if response_message["HEAD"].find("\r\nX-ElQuePregunta") == -1:
-            response_message["HEAD"] += f"\r\nX-ElQuePregunta: {name}"
+        
+        proxy_socket.send(creator.create_HTTP_message(recv_message))
 
-        new_socket.send(creator.create_HTTP_message(response_message))
+        proxy_response_message = proxy_socket.recv(4092)
+        # if response_message["HEAD"].find("\r\nX-ElQuePregunta") == -1:
+        #     response_message["HEAD"] += f"\r\nX-ElQuePregunta: {name}"
 
+        new_socket.send(proxy_response_message)
         new_socket.close()
+        proxy_socket.close()
         print(f"conexión con {new_socket_address} ha sido cerrada")
