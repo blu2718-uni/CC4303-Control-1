9ec3b4fc2702d76d28e902b4439bff77c3729796
Julio Yáñez <julio.yanez@ug.uchile.cl>
2026-09-01 13:01:09 -0400

    feat(creator): the creator now handles a body comprised of raw bytes

--- creator.py
+++ creator.py
@@ -3,8 +3,10 @@ import socket
 def create_HTTP_message(message):
     head = message["HEAD"]
     body = message["BODY"]
-
-    to_encode = head + "\r\n\r\n" + body
-    encoded = to_encode.encode()
+    separator = "\r\n\r\n"
+    if isinstance(body, str):
+        body = body.encode("utf-8")
+    
+    encoded = head.encode("utf-8") + separator.encode("utf-8") + body
 
     return encoded
