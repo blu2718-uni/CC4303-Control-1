cd529b755f5d598a5a372b6c89a5264ad7688644
0scar76 <oscarconejerosb@gmail.com>
2026-09-01 12:41:47 -0400

    feat: the parser can now block forbidden words defined in json_nombre.json

--- actividad_1/server.py
+++ actividad_1/server.py
@@ -4,17 +4,14 @@ import sys
 import creator
 import parser
 
-SERVER_IP="arenarium"
+SERVER_IP="172.20.10.4"
 SERVER_PORT=8000
 BUFFER_SIZE=5
 
-PROXY_HOST="example.com"
-PROXY_PORT=80
-
-with open("html_response.html", "r") as file:
+with open("actividad_1/html_response.html", "r") as file:
     html = file.read()
 
-server_response = {"HEAD":f"HTTP/1.1 200 OK\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length: {len(html.encode("utf-8"))}","BODY":html}
+server_response = {"HEAD":f"HTTP/1.1 403 Forbidden\r\nContent-Type: text/html; charset=UTF-8\r\nContent-Length: {len(html.encode("utf-8"))}","BODY":html}
 
 def receive_full_message(socket, buffer_size):
     recv_message = socket.recv(buffer_size) 
@@ -37,31 +34,69 @@ if __name__ == "__main__":
     server_socket.bind(new_socket_address)
     server_socket.listen(3)
 
-    # json_name = "json_nombre.json"
-    # json_address = ""
+    json_name = "json_nombre.json"
+    json_address = "actividad_1/"
 
-    # if len(sys.argv) == 2:
-    #     json_name = sys.argv[1]
-    # elif len(sys.argv) == 3:
-    #     json_name = sys.argv[1]
-    #     json_address = sys.argv[2]
-    # elif len(sys.argv) > 3:
-    #     print("Mas argumentos que lo esperado, usando valores por defecto")
+    if len(sys.argv) == 2:
+        json_name = sys.argv[1]
+    elif len(sys.argv) == 3:
+        json_name = sys.argv[1]
+        json_address = sys.argv[2]
+    elif len(sys.argv) > 3:
+        print("Mas argumentos que lo esperado, usando valores por defecto")
 
-    # with open(json_address + json_name) as file:
-        # name = json.load(file)["nombre"]
+    with open(json_address + json_name) as file:
+        json = json.load(file)
 
     while True:
         new_socket, new_socket_address = server_socket.accept()
-        proxy_socket.connect((PROXY_HOST, PROXY_PORT))
-        
+        block = False
+        PROXY_PORT=80
         recv_message = receive_full_message(new_socket, buffer_size)
         
         print(f' -> Se ha recibido el siguiente mensaje: {recv_message["HEAD"]}')
+        PROXY_HOST=""
+        host_start = recv_message["HEAD"].find("Host: ") + 6
+        current = host_start
+    
+        while recv_message["HEAD"][current] != "\r":
+            PROXY_HOST += recv_message["HEAD"][current]
+            current += 1
         
-        proxy_socket.send(creator.create_HTTP_message(recv_message))
+        route_start = recv_message["HEAD"].find("http://") + 7 + len(PROXY_HOST)
+        if recv_message["HEAD"][route_start + 1] == " ":
+            to_block = PROXY_HOST
+        else:
+            route = ""
+            current_route = route_start
+            while recv_message["HEAD"][current_route] != " ":
+                route += recv_message["HEAD"][current_route]
+                current_route += 1
+                to_block = PROXY_HOST + route
+        
+        proxy_socket.connect((PROXY_HOST, PROXY_PORT))
+        
+        for blocked in json["blocked"]:
+            if to_block == blocked:
+                block = True
 
-        proxy_response_message = proxy_socket.recv(4092)
+        print(block)
+        
+        if not block:
+            proxy_socket.send(creator.create_HTTP_message(recv_message))
+            proxy_to_parse_message = proxy_socket.recv(4092)
+
+            parsed_proxy_message = parser.parse_HTTP_message(proxy_to_parse_message)
+            # ciclo que cambia las forbidden words
+            for forbidden in json["forbidden_words"]:
+                keys = forbidden.keys()
+                for key in keys:
+                    parsed_proxy_message["BODY"] = parsed_proxy_message["BODY"].replace(key, forbidden[key])
+                    print(parsed_proxy_message["BODY"])
+            proxy_response_message = creator.create_HTTP_message(parsed_proxy_message)
+        else:
+            proxy_response_message = creator.create_HTTP_message(server_response)
+
         # if response_message["HEAD"].find("\r\nX-ElQuePregunta") == -1:
         #     response_message["HEAD"] += f"\r\nX-ElQuePregunta: {name}"
 
