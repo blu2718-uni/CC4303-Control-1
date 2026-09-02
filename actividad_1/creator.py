1947537b48c2d205b0c510358c75aacfb73bc3ca
Julio Yáñez <julio.yanez@ug.uchile.cl>
2026-09-02 12:19:59 -0400

    chore: removed stale files

--- actividad_1/creator.py
+++
@@ -1,8 +0,0 @@
-def create_HTTP_message(message):
-    head = message["HEAD"]
-    body = message["BODY"]
-
-    to_encode = head + "\r\n\r\n" + body
-    encoded = to_encode.encode()
-
-    return encoded
